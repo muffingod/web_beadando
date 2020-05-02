16aee130de79d11111616b2b1b9b38fe910d9275
Antal Norbert <norbikeksz@gmail.com>
2020-05-02 13:30:49 +0200

    fancy validation

--- pages/message.tpl.php
+++ pages/message.tpl.php
@@ -21,23 +21,47 @@ if(@$_POST['email'] != null && @$_POST['message'] != null){
 ?>
 
 <script type="text/javascript">
-    function validate(){
+    function validate_email(){
         var pattern = /^([A-Za-z0-9_∖-∖.])+@([A-Za-z0-9_∖-∖.])+.([A-Za-z]{2,4})$/;
         var email=String(document.getElementById("email").value);
         if(email.match(pattern) == null ){
-            alert("Hibás e-mail!");
+            document.getElementById("email").style.borderColor = "red";
+            document.getElementById("message").style.borderWidth = "2px";
+            return false;
+        }else{
+            document.getElementById("email").style.borderColor = "white";
+            return true;
         }
+    }
 
-        if(document.getElementById("message").value.length == 0 ){
-            alert("Az üzenet nem lehet üres!");
+    function validate_message(){
+        if(document.getElementById("message").value.length == 0 || document.getElementById("message").value.length >=140){
+            document.getElementById("message").style.borderColor = "red";
+            document.getElementById("message").style.borderWidth = "2px";
+            return false;
+        }else{
+            document.getElementById("email").style.borderColor = "white";
+            return true;
         }
+    }
+
+
+    function validate(){
+        validate_email();
+        validate_message();
+        if(!validate_email() || !validate_message()){
+            return false;
+        }else{
+            return true;
+        }
+        
     }
 </script>
 
-<form action="" method="post">
-    <input id="email" type="email" name="email" id="" placeholder="E-mail cím" ><br>
+<form action="" method="post" onsubmit="return validate();">
+    <input id="email" type="email" name="email" id="" placeholder="E-mail cím"><br>
     <input type="checkbox" name="ishungarian" id="">
     <label for="ishungarian">Magyar</label></br>
-    <textarea id="message" name="message" id="" cols="30" rows="10" ></textarea></br>
-    <input id="submit" type="submit" value="Küldés" onclick="validate()">
+    <textarea id="message" name="message" id="" cols="30" rows="10"></textarea></br>
+    <input id="submit" type="submit" value="Küldés">
 </form>
