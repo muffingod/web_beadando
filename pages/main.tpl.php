de987f88461ac1cf041366edc2911f73896d4747
Kristóf Vogyák <muffingod001@gmail.com>
2020-05-05 14:04:09 +0200

    Plus content

--- pages/main.tpl.php
+++ pages/main.tpl.php
@@ -2,12 +2,24 @@
 <h2>A tanulás élmény</h2>
 
 <div>Légy részese te is az élménynek a kiterjesztett valóság segítségével.</div>
-
+<p>
+    A mai fiatal generációnak már természetes az okostelefon jelenléte és használata, a benne rejlő potenciált mégsem használják ki ezt olyan területeken, mint például az oktatás.
+</p>
+<p>
+    Csapatunk magas fejlődési potenciállal büszkélkedő összefogás, innovatív szellemi tőkével és technológiával. Célunk, hogy a közeljövőben olyan startupot alapítsunk, amely nagy növekedési potenciájával és fiatalos gondolkodásmódjával nagy növekedési potenciájával és fiatalos gondolkodásmódjával felkelti a felhasználók és a befektetők figyelmét.
+</p>
+<p>
+    Kijelenthető, hogy a fiatalok nagyon nagy része napi szinten aktívan használ okostelefont. Meggyőződésünk, hogy ezek a készülékek a tanulási folyamatot sokkal érdekesebbé, sokkal élményszerűbbé, és sokkal interaktívabbá tehetik a tanulók számára a jelenkor modern technológiáival.
+</p>
 <h2>Diákoknak</h2>
-<p>Te is unod a száraz tankönyveket? Nem vagy egyedül! Éld át a kiterjesztett valóság adta élményt,
-és tudj meg többet a tananyagról, mint amit a könyvek adhatnak.</p>
+<p>
+    Te is unod a száraz tankönyveket? Nem vagy egyedül! Éld át a kiterjesztett valóság adta élményt,
+    és tudj meg többet a tananyagról, mint amit a könyvek adhatnak.
+</p>
 <h2>Szülőknek</h2>
-<p>Mit gondolsz, a telefon és az iskola lehetnek barátok? Az Eddie célja, hogy a diákok tanulási
-folyamatát élményszerűbbé tegye a modern technológiák és a gyerekek mai ingerküszöbéhez méltó megoldásokkal.</p>
+<p>
+    Mit gondolsz, a telefon és az iskola lehetnek barátok? Az Eddie célja, hogy a diákok tanulási
+    folyamatát élményszerűbbé tegye a modern technológiák és a gyerekek mai ingerküszöbéhez méltó megoldásokkal.
+</p>
 
 <video width="320" height="240" autoplay loop>   <source src="./videos/eddie.mp4" type="video/mp4">   </video>
